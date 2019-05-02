561d07903b610d7cf1f4f19f8cda4accdb00fbbc
Gustavo Viegas <webviegas@gmail.com>
2019-05-02 17:24:24 -0300

    Ajustes commander

--- src/commander.cpp
+++ src/commander.cpp
@@ -1,7 +1,23 @@
+#import "Comander.hpp"
+inc
 
 // Constructor
 Commander::Commander(arguments) {
 	// Cria um pipe
-	pipe = Pipe()
+	this->pipe = Pipe();
 
+	// Cria um PM
+	this->processManager = ProcessManager();
+}
+
+
+void menu(void) {
+}
+
+string leArquivo(void) {
+	return "not_implemented";
+}
+
+string leEntrada(void) {
+	return "not_implemented";
 }
