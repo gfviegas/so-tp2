f632229c611a564e51e7f1ce677c3bdeaf1bd8d4
Bruno Marra de Melo <brunomarrademelo@MacBook-Pro-de-Bruno.local>
2019-05-06 19:28:14 -0300

    Adianta processo simuladoe

--- src/simulatedProcess.hpp
+++ src/simulatedProcess.hpp
@@ -1,29 +1,36 @@
 #ifndef simulatedProcess_hpp
 #define simulatedProcess_hpp
+
 #include <string>
+#include "bits/stdc++.h"
+
+static int COUNT_ID = 0;
 
 using namespace std;
 
-class SimulatedProcess {
-	private:
-		/* data */
+class SimulatedProcess
+{
+private:
+	int id;
+	int pc;
+	int n;
+	bool blocked;
+	string *program;
 
-	public:
-		int id;
-		int pc;
-		int n;
-		string program;
+	void setProgram(string *p);
 
-		SimulatedProcess (void);
+public:
+	SimulatedProcess();
+	SimulatedProcess(string rawProgram);
 
-		string readComand(void);
-		void set(int value);
-		void add(int value);
-		void sub(int value);
-		void block(void);
-		void end(void);
-		void fork(void);
-		void read(void);
+	string readComand(void);
+	void set(int value);
+	void add(int value);
+	void sub(int value);
+	void block(bool block);
+	void end(SimulatedProcess **process);
+	SimulatedProcess *fork(void);
+	void read(void);
 };
 
 #endif /* simulatedProcess_hpp */
